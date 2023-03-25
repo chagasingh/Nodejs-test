7a4536444f91086a828700e749a792bdce4119f3
chagansingh <chagansingh9876@gmail.com>
2023-03-25 07:38:32 +0530

    object and fat arrow

--- node.js
+++ node.js
@@ -1,1 +1,16 @@
 console.log('hellow World')
+let a=3
+let b= 4;
+
+const product=(x,y)=>x*y;
+console.log(product(a,b))
+
+const student={
+    name:'ram',
+    age:22,
+    college() {
+        console.log(`${this.name} college is very good`)
+    }
+}
+
+student.college()
