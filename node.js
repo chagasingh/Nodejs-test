ec854ae663783ff3d6688fdbe9f9a2f0e49eb995
chagansingh <chagansingh9876@gmail.com>
2023-03-25 22:48:09 +0530

    server changed when changing page url

--- node.js
+++ node.js
@@ -1,18 +1,29 @@
 const http = require('http')
 
-// function rqlistner(req,res){
 
-// }
-
-// http.createServer(rqlistner);
-
-// http.createServer(function(req,res){
-
-// });
 
 const server = http.createServer((req,res)=>{
-    console.log('chagan singh')
-    console.log(req)
+    const url = req.url;
+    res.setHeader('Content-Type','text/html');
+    if (url === '/home'){
+        res.write('<html>');
+        res.write('<body><h1>welcome to home page</h1></body>');
+        res.write('/<html>');
+        return res.end();
+    }
+    else if (url === '/about'){
+        res.write('<html>');
+        res.write('<body><h1>welcome to about page</h1></body>');
+        res.write('/<html>');
+        return res.end();
+    }
+    else {
+        res.write('<html>');
+        res.write('<body><h1>welcome to node js happy coding</h1></body>');
+        res.write('/<html>');
+        return res.end();
+    }
+    
 });
 
-server.listen(4000)
+server.listen(3000)
